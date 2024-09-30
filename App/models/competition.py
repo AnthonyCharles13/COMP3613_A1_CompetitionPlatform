b154ed07742ec8cfae58c78931bf5296a8bd152c
AnthonyCharles13 <orlandosahadeo13@gmail.com>
2024-09-30 16:17:13 +0000

    modified:   App/models/__init__.py 	modified:   App/models/competition.py

--- App/models/competition.py
+++ App/models/competition.py
@@ -1,3 +1,24 @@
 from App.database import db
 
+class Competition(db.Model):
+    compID = db.Column(db.Integer, primary_key=True)
+    compName = db.Column(db.String(255), nullable=False)
+    location = db.Column(db.String(255), nullable=False)
+    date = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, compName, location, date):
+        self.compName = compName
+        self.location = location
+        self.date = date
+
+    def get_json(self):
+        return {
+            'compID': self.compID,
+            'compName': self.compName,
+            'location': self.location,
+            'date': self.date
+        }
+
+    def __repr__(self):
+        return f'<Competition ID: {self.compID} | Name: {self.compName} | Location: {self.location} | Date: {self.date}>'
 
